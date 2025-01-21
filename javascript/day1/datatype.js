7f758ad38f591f828ff2df05e0849ebfff83798a
Joy-tii <157490488+Joy-tii@users.noreply.github.com>
2025-01-21 19:45:47 +0530

    hh

---
+++ javascript/day1/datatype.js
@@ -0,0 +1,45 @@
+
+
+var a = 10;
+console.log("this is ", a);
+console.log(typeof a);
+
+var x = "jyoti";
+console.log("This is ",x);
+console.log(typeof x);
+
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+let mark;
+console.log(mark);
+console.log(typeof mark)
+
+let marks = null;
+console.log(marks);
+console.log(typeof marks)
+
+let sym = Symbol("id");
+console.log(sym);
+console.log(typeof sym);
+
+let bigInt = 112334445566899087746353526328349745955647443n;
+console.log(bigInt);
+console.log(typeof bigInt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
