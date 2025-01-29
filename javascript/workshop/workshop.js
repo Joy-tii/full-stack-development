903a72455745b5be6c495158f1b6b10108306ac9
Joy-tii <157490488+Joy-tii@users.noreply.github.com>
2025-01-29 18:56:20 +0530

    hh

--- javascript/workshop/workshop.js
+++ javascript/workshop/workshop.js
@@ -3,18 +3,23 @@ function password(){
 
  if(password.length < 8){
     console.log("password must be minimum 8 character.");
+    alert("password must be minimum 8 character.");
  }
 
  else if (!/[A-Z]/.test(password)){
     console.log("Password must have one uppercase .");
+    alert("Password must have one uppercase .");
  }
 
  else if (!/[0-9]/.test(password)){
     console.log("Password must have one number .");
+   alert("Password must have one number .");
  } else{
     console.log("Password created Successfully");
+    alert("Password created Successfully");
+}
  }
  
-}
+
 
 password();
