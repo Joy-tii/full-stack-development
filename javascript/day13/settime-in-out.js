df97e1f713f842857c80fc447f9924b02670202e
Joy-tii <157490488+Joy-tii@users.noreply.github.com>
2025-02-07 02:36:17 +0530

    hh

---
+++ javascript/day13/settime-in-out.js
@@ -0,0 +1,27 @@
+// console.log("/hello!!!")
+
+// function Test(){
+//     console.log("this is test para");
+// }
+
+// const test = () => {
+//     console.log("this is test para");
+// };
+// setTimeout(test, 3000)
+
+// setTimeout(() => { console.log("this is test para....") }, 3000)
+
+// for(let i=1; i<=5; i++){
+// setTimeout(() => {
+// console.log("counting: ", i);
+// } , i*1000);
+// }
+
+// setInterval(() => {
+//     console.log("This sentence will be printed on every 2 seconds..");
+// }, 2000);
+
+
+
+
+
